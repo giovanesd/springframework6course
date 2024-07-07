f6bf7ef00be8fc92156071c59b15906e5329a76d
giovanesd <giovanesd@gmail.com>
2024-07-07 00:16:09 -0300

    Adding book controller.

---
+++ src/main/java/com/courses/springframework6webapp/controllers/BookController.java
@@ -0,0 +1,37 @@
+/**
+ * Created by Giovane, 7 de jul. de 2024.
+ */
+package com.courses.springframework6webapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import com.courses.springframework6webapp.services.BookService;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+/**
+ * 
+ */
+@Controller
+public class BookController {
+
+	private final BookService bookService;
+
+	/**
+	 * @param bookService
+	 */
+	public BookController(BookService bookService) {
+		this.bookService = bookService;
+	}
+	
+	
+	@RequestMapping("/books")
+	public String getBooks(Model model) {
+		model.addAttribute("books", this.bookService.findAll());
+		return "books";
+	}
+}
